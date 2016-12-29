c3269339508ffa4e54a31388ee85708b9548ce0d
Dhinesh Kumar <DhineshAJ@users.noreply.github.com>
2016-12-29 13:58:53 +0530

    initial commit

---
+++ Chrome/background.js
@@ -0,0 +1,26 @@
+function getDomain(name){
+switch (name) {
+    case "Google":
+      return "http://www.google.com/";
+  case "Facebook":
+	return "http://www.facebook.com/";
+  case "Twitter":
+	return "https://www.twitter.com";
+
+	}
+
+}
+
+function openMyPage(message) {
+var domain = getDomain(message.name);
+  chrome.tabs.create({
+     "url": domain
+   });
+}
+
+
+
+
+
+
+chrome.runtime.onMessage.addListener(openMyPage);
---
+++ Chrome/popup/choose_beast.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function(e) {
+  if (!e.target.classList.contains("beast")) {
+    return;
+  }
+
+  var name = e.target.textContent;
+
+	 chrome.runtime.sendMessage({"name": name});
+});
